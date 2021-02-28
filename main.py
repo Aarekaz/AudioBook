2743baeb17b15808ec81c01676ad0a24fb11f6fc
Anurag Dhungana <anuragdhungana1@gmail.com>
2021-02-28 11:49:41 +0545

    Finished the Code

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pyttsx3
+import PyPDF2
+
+book = open('test.pdf', 'rb')  # reading the book as a binary book
+reader = PyPDF2.PdfFileReader(book)
+pages = reader.numPages
+print(pages)
 speaker = pyttsx3.init()  # Initializing the library
+for num in range(7,pages):
+    page = reader.getPage(1)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
 
-speaker.say('Hello world')
-
-speaker.runAndWait()
